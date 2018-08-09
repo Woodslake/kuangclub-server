7e9acf6856a4a9e05334f6d252f8ef142d5cd63b
Woodslake <woodslakelee@gmail.com>
2018-08-09 19:51:31 +0800

    ...

--- src/main/java/com/kuangclub/controller/SocketController.java
+++ src/main/java/com/kuangclub/controller/SocketController.java
@@ -1,4 +1,34 @@
 package com.kuangclub.controller;
 
+import com.kuangclub.service.WebSocketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
 public class SocketController {
+
+    private int i = 0;
+
+    @Autowired
+    private WebSocketService webSocketService;
+
+    @RequestMapping("/sendMessage")
+    public String sendMessage(){
+        try {
+            webSocketService.sendInfo("i = " + i);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "sendMessage:" + i++;
+    }
+
+    @RequestMapping("/clear")
+    public String clear(){
+        i = 0;
+        return "sendMessage:" + i++;
+    }
+
 }
