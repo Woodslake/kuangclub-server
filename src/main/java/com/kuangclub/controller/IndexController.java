745c148656c9f47b968fbc6915af540a66d0839a
Woodslake <woodslakelee@gmail.com>
2018-08-13 12:33:01 +0800

    ...

--- src/main/java/com/kuangclub/controller/IndexController.java
+++ src/main/java/com/kuangclub/controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @RequestMapping("")
-    public String hello(){
+    public String index(){
         return "index";
     }
 
